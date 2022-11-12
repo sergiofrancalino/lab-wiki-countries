4169fafd9d54cb1f23686e19cd37bfffcf0c9afe
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-12 17:53:24 -0300

    Done 0-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
     <div className="App">
-    
+
       <Navbar />
 
       <div className="container">
           <div className="row">
-             <CountriesList countries={countries} />
+             <CountriesList countries={CountriesList} />
              {/* React-Router Route rendering the CountryDetails should go here */}
          </div>
       </div>
